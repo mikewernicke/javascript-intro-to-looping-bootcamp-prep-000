8834047e9282ac6ae98c002e3a1e93b137f16ddb
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-21 15:31:20 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+var array = [];
+
+function forLoop(array) {
+    for (var i = 0; i < 25; i++) {
+      var endSentence = 's.'
+      if (i == 1) endSentence = "."
+      array.push("I am ${i} strange loop${endSentence}")
+    }
+    return array
+}
+function whileLoop(n) {
+  while (n>0) {
+    console.log(--n)
+  }
+  return 'done'
+}
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue())
+  return array;
+}
