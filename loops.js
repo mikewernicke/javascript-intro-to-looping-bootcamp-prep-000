4a3e218c9612db6781aa015cfa1f85d3cf51c5a6
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-21 15:22:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,7 +19,7 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5;
   }
 //  do {
-//    array.slice(0, array.length - 1);
+    array.slice(0, array.length - 1);
 //  } while array.length > 0 && (maybeTrue())
   return array;
 }
